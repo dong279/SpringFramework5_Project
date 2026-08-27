d49221dcec985c34b3ecde571124e5beaeacc167
dong279 <dongho0130@daum.net>
2026-08-27 16:31:47 +0900

    HelloBeanSpringTest update

--- src/test/java/myspring/di/xml/HelloBeanSpringTest.java
+++ src/test/java/myspring/di/xml/HelloBeanSpringTest.java
@@ -1,9 +1,11 @@
  package myspring.di.xml;
 
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "classpath:hello-bean.xml")
 public class HelloBeanSpringTest {
 	
 }

44174f764f866d3e4573855d23a72fb2ea1a3e01
dong279 <dongho0130@daum.net>
2026-08-27 16:43:49 +0900

    HelloBeanSpringTest update

--- src/test/java/myspring/di/xml/HelloBeanSpringTest.java
+++ src/test/java/myspring/di/xml/HelloBeanSpringTest.java
@@ -3,6 +3,7 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -10,10 +11,11 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ContextConfiguration(locations = "classpath:hello-bean.xml")
 public class HelloBeanSpringTest {
 	@Autowired
+	@Qualifier("helloC")
 	Hello hello;
 	
 	@Test
 	void helloBeanCons(){
-		
+		System.out.println(hello.sayHello());
 	}
 }
